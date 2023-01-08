addff46b6831f97675029094f28b04e52c27f443
obricks <obricks21@gmail.com>
2023-01-08 19:01:13 +0900

    로그인 처리

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,9 +4,10 @@ const id = document.querySelector('#id'),
 password = document.querySelector('#password'),
 loginBtn = document.querySelector('button');
 
-
+//로그인 버튼 클릭 시 실행
 loginBtn.addEventListener('click', login);
 
+//로그인 기입 정보를 json으로 post 전송
 function login(){
     const req = {
         id: id.value,
@@ -19,6 +20,16 @@ function login(){
         },
         body: JSON.stringify(req),
     })
-    .then((res) => res.json())
-    .then(console.log);
+    .then((res) => res.json())  //서버에서 res된 값을 받는다.
+    .then((res) => {            //서버에서 res된 값에 대해 검증하고 이에 따른 실행문
+        if (res.success){
+            alert ("로그인 성공");
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {  //에러 처리
+        console.error(new Error("로그인 중 에러 발생"));
+    });
 }
