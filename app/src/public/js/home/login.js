2e00e6e68d23dcd432ec6bc6abfb495b31103632
obricks <obricks21@gmail.com>
2023-01-08 18:12:59 +0900

    로그인을 요청하는 fecth 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,13 @@ function login(){
         id: id.value,
         password: password.value,
     };
-    console.log(req);
+    console.log (req);
+    console.log (JSON.stringify(req));
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
 }
