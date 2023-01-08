8d44e06700f55dda04fc4748e238c60fc01e3349
obricks <obricks21@gmail.com>
2023-01-08 17:23:59 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("../src/routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
 app.set("views", "./src/views");
